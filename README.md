2936fd5fe2f175d621a84603d00166c04b6443bd
Benjamin Georgeault <benjamin@pressop.eu>
2019-07-10 12:56:54 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+Todo List API
+=============
+
+## Install
+
+- Clone this repository
+- Run `composer install`
+- Create a `.env.local` file on project root directory
+- Configure your database env variable in `.env.local`
+- Run `php bin/console doctrine:database:create` to create the database
+- Run `php bin/console doctrine:migration:migrate` to apply the database schema
+- Run `php bin/console hautelook:fixtures:load --purge-with-truncate` to populate the database with fixtures
+
+## Run the server
+
+`php bin/console server:run`
+
+## License
+
+
---
+++ src/Entity/Todo.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TodoRepository")
+ */
+class Todo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $finish;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $priority;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getFinish(): ?bool
+    {
+        return $this->finish;
+    }
+
+    public function setFinish(bool $finish): self
+    {
+        $this->finish = $finish;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(?int $priority): self
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
